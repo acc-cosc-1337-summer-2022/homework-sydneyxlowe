3ca7c6979d679de1b4a640b44a1a4c0eb69b84b4
sydneyxlowe <sydneyjlowe@yahoo.com>
2022-06-25 15:48:26 +0000

    notes_awhileago

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,15 @@
+#include<iostream>
+using std::cout;
+
 int main()
 {
+    int num = 5;
+    cout<<num<<"\n";
+    cout<<"size of num: "<<sizeof(num)<<"\n";
+    cout<<"memory: "<<&num<<"\n";
+
+    int &num_ref = num;
+    cout<<"num ref value: "<<num_ref<<"\n";
+
     return 0;
 }
--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,15 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "void.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("test value parameters generate a copy")
+{
+	int num = 5;
+	value_param(num);
+
+	REQUIRE(num == 5);
 }
